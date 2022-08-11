cac10d1d08b53287fcf94e5cf17bc6c7bc35bb99
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-08-11 11:18:38 +0600

    radio

---
+++ template-parts/radio-player/home/hero-area.php
@@ -0,0 +1,27 @@
+
+<section class="integrate-google-drive-hero-area">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-7 m-auto">
+                <div class="hero-content text-center">
+                    <h1>Complete <span>Google Drive</span> Cloud Solution for WordPress.</h1>
+                    <p>
+                        Integrate Google Drive – is the best and easy-to-use Google Drive cloud solution plugin for WordPress to integrate your Google Drive documents and media directly into your WordPress Website.
+                    </p>
+                    <a href="#" class="get">Get Started</a>
+                    <a href="#" class="get1">Check Demo</a>
+                </div>
+            </div>
+        </div>
+
+                <!--=== google drive ===--->
+               <div class="row">
+                <div class="col-lg-8 m-auto">
+                    <div class="inner-header-bg text-center">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/google-drive/inner-header-bg.png" alt="drive-head-icon1">
+                    </div>
+                </div>
+               </div>
+
+    </div>
+</section>
---
+++ templates/radio-player.php
@@ -0,0 +1,18 @@
+
+
+<?php
+/**
+ * Template Name: radio-player-Page
+ *
+ */
+
+get_header('integrate-google-drive');
+
+
+get_template_part( 'template-parts/radio-player/home/hero-area' );
+?>
+
+
+
+<?php
+get_footer('integrate-google-drive');
