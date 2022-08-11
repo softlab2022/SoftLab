5df6e064bce470d9ca0c98a2f2bf18a38de941e7
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-08-11 14:31:02 +0600

    az

--- template-parts/radio-player/home/hero-area.php
+++ template-parts/radio-player/home/hero-area.php
@@ -2,26 +2,25 @@
 <section class="radio-player-hero-area">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-7 m-auto">
+            <div class="col-lg-6 m-auto">
                 <div class="hero-content text-center">
-                    <h1>Complete <span>Google Drive</span> Cloud Solution for WordPress.</h1>
+                    <span>Radio Player</span> 
+                    <h1>Live Shoutcast, Icecast and Audio Stream Player for WordPress</h1>
                     <p>
-                        Integrate Google Drive – is the best and easy-to-use Google Drive cloud solution plugin for WordPress to integrate your Google Drive documents and media directly into your WordPress Website.
+                    A simple, easy to use, user-friendly and fully customizable web radio player for WordPress. You can play any live mp3, iceCast and Shoutcast stream in your WordPress website.
                     </p>
                     <a href="#" class="get">Get Started</a>
                     <a href="#" class="get1">Check Demo</a>
                 </div>
             </div>
+            <div class="col-lg-6">
+            <div class="radio-header-bg text-center">
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/google-drive/radio-img.png" alt="drive-head-icon1">
+                    </div>
+            </div>
         </div>
 
-                <!--=== google drive ===--->
-               <div class="row">
-                <div class="col-lg-8 m-auto">
-                    <div class="inner-header-bg text-center">
-                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/google-drive/inner-header-bg.png" alt="drive-head-icon1">
-                    </div>
-                </div>
-               </div>
+          
 
     </div>
 </section>
