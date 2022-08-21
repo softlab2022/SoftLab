214afd3c38e5e115d8135585a5c40512b7a23bc0
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-08-21 10:08:24 +0600

    hhhhh

--- templates/integrate-google-drive-media-player.php
+++ templates/integrate-google-drive-media-player.php
@@ -8,7 +8,6 @@
 
 get_header('integrate-google-drive');
 
-
 get_template_part( 'template-parts/integrate-google-drive/demo/media-demo-hero' );
 get_template_part( 'template-parts/integrate-google-drive/demo/media-demo-detalis' );
 get_template_part( 'template-parts/integrate-google-drive/home/integrate' );
