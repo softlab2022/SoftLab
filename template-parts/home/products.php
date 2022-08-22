2f91b36b361a1074065f082d1897ede67351a4b7
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-08-22 10:09:41 +0600

    add site logo

--- template-parts/home/products.php
+++ template-parts/home/products.php
@@ -18,46 +18,52 @@
         
         <div class="row">
             <div class="col-lg-4 col-md-6 m-auto">
+                <a href="https://softlabbd.com/wp-radio/">
                 <div class="products-item text-center">
                     <div class="porducts-icon">
-                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-icon.png" alt="wp-icon"></a>
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/products/wp-radio-icon.png" alt="wp-radio-icon">
                         </div>                    
                         <div class="products-name">
-                       <a href="#">WP Radio</a>
+                           <h5> WP Radio</h5>
                         <p>WP Radio – Worldwide Online Radio Stations Directory for WordPress.</p>
                         <div class="products-butt">
-                        <a href="http://softlab.local/wp-radio/" >Explore <i class="fa-solid fa-arrow-right"></i></a>
+                        <span>Explore <i class="fa-solid fa-arrow-right"></i></span>
                         </div>
                     </div>
                 </div>
+                </a>
             </div>
             <div class="col-lg-4 col-md-6 m-auto">
+                <a href="https://softlabbd.com/radio-player/">
                 <div class="products-item text-center">
                     <div class="porducts-icon">
-                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/raido-player-icon.png" alt="wp-icon"></a>
+                        <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/products/radio-icon.png" alt="radio-icon">
                         </div>                    
                         <div class="products-name">
-                        <a href="#">radio player</a>
+                            <h5>radio player</h5>
                         <p>A simple, easy to use, user-friendly and fully customizable web radio player.</p>
                         <div class="products-butt">
-                        <a href="https://softlabbd.com/radio-player/" >Explore <i class="fa-solid fa-arrow-right"></i></a>
+                        <span>Explore <i class="fa-solid fa-arrow-right"></i></span>
                         </div>
                     </div>
                 </div>
+                </a>
             </div>
             <div class="col-lg-4 col-md-6 m-auto">
+                <a href="https://softlabbd.com/integrate-google-drive/">
                 <div class="products-item text-center">
                     <div class="porducts-icon">
-                    <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/google-drive-icon.png" alt="wp-icon"></a>
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/products/integrate-google-drive-icon.png" alt="integrate-google-drive-icon">
                     </div>
                     <div class="products-name">
-                        <a href="#">Integrate Google Drive</a>
+                        <h5>Integrate Google Drive</h5>
                         <p>Integrate Google Drive – is the best and easy-to-use Google Drive cloud solution.</p>
                         <div class="products-butt">
-                        <a href="https://softlabbd.com/integrate-google-drive/" >Explore <i class="fa-solid fa-arrow-right"></i></a>
+                        <span>Explore <i class="fa-solid fa-arrow-right"></i></span>
                         </div>
                     </div>
                 </div>
+                </a>
             </div>
 
         </div>
--- template-parts/wp-radio/home/addons.php
+++ template-parts/wp-radio/home/addons.php
@@ -13,47 +13,55 @@
 
         <div class="row">
             <div class="col-lg-3 m-auto">
-                <div class="addon-item text-center">
-                <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/user.png" alt="user">
-                <div class="add">
-                <span>WP Radio User Frontend</span>
-                <p>Let engage your user to your website by adding powerful features...</p>
-                <a href="/user-frontend/">View Details <i class="fa-solid fa-arrow-right"></i></a>
-                </div>
-                </div>
+                <a href="/user-frontend/">
+                    <div class="addon-item text-center">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/user.png" alt="user">
+                    <div class="add">
+                    <span>WP Radio User Frontend</span>
+                    <p>Let engage your user to your website by adding powerful features...</p>
+                    <h5>View Details <i class="fa-solid fa-arrow-right"></i></h5>
+                    </div>
+                    </div>
+                </a>
             </div>
 
             <div class="col-lg-3 m-auto">
-                <div class="addon-item add-item1 text-center">
-                <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/proxy.png" alt="proxy">
-                <div class="add">
-                <span>WP Radio Proxy Player</span>
-                <p>As of Google Chrome browser updates with some restriction...</p>
-                <a href="/proxy-player/">View Details <i class="fa-solid fa-arrow-right"></i></a>
-                </div>
-                </div>
+                <a href="/proxy-player/">
+                    <div class="addon-item add-item1 text-center">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/proxy.png" alt="proxy">
+                    <div class="add">
+                    <span>WP Radio Proxy Player</span>
+                    <p>As of Google Chrome browser updates with some restriction...</p>
+                    <h5>View Details <i class="fa-solid fa-arrow-right"></i></h5>
+                    </div>
+                    </div>
+                </a>
             </div>
 
             <div class="col-lg-3 m-auto">
-                <div class="addon-item add-item2 text-center">
-                <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/ads.png" alt="ads">
-                <div class="add">
-                <span>WP Radio Ads Player</span>
-                <p>Monetize your radio directory website by playing custom ..</p>
-                <a href="/wp-radio-station/">View Details <i class="fa-solid fa-arrow-right"></i></a>
-                </div>
-                </div>
+                <a href="/wp-radio-station/">
+                    <div class="addon-item add-item2 text-center">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/ads-player.png" alt="ads-player">
+                    <div class="add">
+                    <span>WP Radio Ads Player</span>
+                    <p>Monetize your radio directory website by playing custom ..</p>
+                    <h5>View Details <i class="fa-solid fa-arrow-right"></i></h5>
+                    </div>
+                    </div>
+                </a>
             </div>
 
             <div class="col-lg-3 m-auto">
-                <div class="addon-item add-item1 text-center">
-                <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/import.png" alt="import">
-                <div class="add">
-                <span>WP Radio Image Import</span>
-                <p>By default, all the imported station's images are loaded from ...</p>
-                <a href="/image-importer/">View Details <i class="fa-solid fa-arrow-right"></i></a>
-                </div>
-                </div>
+                <a href="/image-importer/">
+                    <div class="addon-item add-item1 text-center">
+                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/wp-radio/home/addons/import.png" alt="import">
+                    <div class="add">
+                    <span>WP Radio Image Import</span>
+                    <p>By default, all the imported station's images are loaded from ...</p>
+                    <h5>View Details <i class="fa-solid fa-arrow-right"></i></h5>
+                    </div>
+                    </div>
+                </a>
             </div>
 
         </div>
