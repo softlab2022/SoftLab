6e51fadc57d791d015546b350a094b071e2453b7
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-09-18 10:01:11 +0600

    add wp price feature

--- template-parts/integrate-google-drive/home/testimonial.php
+++ template-parts/integrate-google-drive/home/testimonial.php
@@ -51,22 +51,30 @@ $icons = [ 'icon-green', 'icon-pink', 'icon-purple' ];
         </div>
 
         <div class="row">
-            <div class="testimonial-items">
-				<?php foreach ( $testimonials as $testimonial ) { ?>
-                    <div class="col-md-3 m-auto">
-                        <div class="testimonial-item">
-                            <div class="icon <?php echo $icons[ array_rand( $icons ) ]; ?>">
-                                <i class="fa-solid fa-quote-left"></i>
-                            </div>
-                            <p><?php echo $testimonial['description']; ?></p>
+                
+            <div class="testimonial">
+                <div class="arrows">
+                        <i class= "fa-solid fa-chevron-left left"></i>
+                        <i class= "fa-solid fa-chevron-right right"></i>
+                    </div>
+                <div class="testimonial-items">
+                
+                    <?php foreach ( $testimonials as $testimonial ) { ?>
+                        <div class="col-md-3 m-auto">
+                            <div class="testimonial-item">
+                                <div class="icon <?php echo $icons[ array_rand( $icons ) ]; ?>">
+                                    <i class="fa-solid fa-quote-left"></i>
+                                </div>
+                                <p><?php echo $testimonial['description']; ?></p>
 
-                            <div class="testimonial-user d-flex align-items-center">
-                                <i class="fa-solid fa-user"></i>
-                                <span><?php echo $testimonial['name']; ?></span>
+                                <div class="testimonial-user d-flex align-items-center">
+                                    <i class="fa-solid fa-user"></i>
+                                    <span><?php echo $testimonial['name']; ?></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-				<?php } ?>
+                    <?php } ?>
+                </div>
             </div>
         </div>
 
