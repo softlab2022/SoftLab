e701356036996839ce4518571da1520d6518e2d5
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-09-21 13:14:37 +0600

    resolve docs page

--- template-parts/documentation/hero.php
+++ template-parts/documentation/hero.php
@@ -17,16 +17,16 @@
             <div class="col-lg-4 m-auto">
                 <div class="docs-item">
                         <div class="docs-logo d-flex justify-content-between align-items-center">
-                            <h6><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/f-wp-radio.png"> Wp Radio</h6>
+                            
+                            <a href="/docs-details-wp-radio/"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/f-wp-radio.png"> Wp Radio</a>
                             <span>12</span>
                         </div>
                     <div class="docs-products">
-                        
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> What is WP Radio?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Install and Use WP Radio ?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to use Shortcodes in website?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Add Unlimited Radio Stations ?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to show Recently Played Tracks Playlist ?</p>
+                        <a href="/what-is-wp-radio-details/"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> What is WP Radio?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Install and Use WP Radio ?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to use Shortcodes in website?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Add Unlimited Radio Stations ?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to show Recently Played Tracks Playlist ?</a>
                     </div>
                     <div class="docs-butt text-center">
                         <a href="/docs-details-wp-radio/">Explore More</a>
@@ -36,16 +36,16 @@
             <div class="col-lg-4 m-auto">
                 <div class="docs-item">
                         <div class="docs-logo docs-player d-flex justify-content-between align-items-center">
-                            <h6><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/footer-radio.png"> Radio Player</h6>
+                           
+                            <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/footer-radio.png"> Radio Player</a>
                             <span>08</span>
                         </div>
                     <div class="docs-products">
-                        
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> What is Radio Player?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Install and Use Radio player ?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to use Shortcodes in website?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Add Unlimited Radio Stations ?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to show Recently Played Tracks Playlist ?</p>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> What is Radio Player?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Install and Use Radio player ?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to use Shortcodes in website?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Add Unlimited Radio Stations ?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to show Recently Played Tracks Playlist ?</a>
                     </div>
                     <div class="docs-butt text-center">
                         <a href="#">Explore More</a>
@@ -55,16 +55,16 @@
             <div class="col-lg-4 m-auto">
                 <div class="docs-item">
                         <div class="docs-logo docs-google-drive d-flex justify-content-between align-items-center">
-                            <h6><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/f-integrate-google-drive.png"> Integrate Google Drive</h6>
+                            
+                            <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/f-integrate-google-drive.png"> Integrate Google Drive</a>
                             <span>14</span>
                         </div>
                     <div class="docs-products">
-                        
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> What is Integrate Google Drive?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to add google account?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to use select folder options?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Add google drive in your website ?</p>
-                        <p><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to show Recently folder visitors ?</p>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> What is Integrate Google Drive?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to add google account?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to use select folder options?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to Add google drive in your website ?</a>
+                        <a href="#"><img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/home/docs/icon.png"> How to show Recently folder visitors ?</a>
                     </div>
                     <div class="docs-butt text-center">
                         <a href="#">Explore More</a>
