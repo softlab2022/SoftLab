d8737449a76103a60cc5bb59ba2a66c96ff3f4da
akash29 <90980196+akashahmed29@users.noreply.github.com>
2022-11-21 15:25:22 +0600

    update headers

--- header.php
+++ header.php
@@ -59,7 +59,7 @@ $search_enabled = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-val
 						);
 						?>
 
-                        <a class="nav-link btn text-white view-products-btn" href="/products/">View Products</a>
+                        <a class="nav-link btn text-white view-products-btn" href="/account/"><i class="fa-sharp fa-solid fa-user"></i> My account</a>
 
                     </div><!-- /.navbar-collapse -->
                 </nav><!-- /#header -->
