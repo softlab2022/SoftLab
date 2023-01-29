072e2e70f1d0b0e88da90668f54523213f376c19
Prince Ahmed <hitprince99@gmail.com>
2023-01-29 17:54:49 +0600

    update pricing

--- template-parts/dracula/pricing/pricing.php
+++ template-parts/dracula/pricing/pricing.php
@@ -205,8 +205,8 @@ $elite_features = [
                                         <span class="price-value lifetime five">99.99</span>
                                         <span class="price-value lifetime hundred">199.99</span>
 
-                                        <span class="price-suffix annual">/ Billed once</span>
-                                        <span class="price-suffix lifetime">/ Year</span>
+                                        <span class="price-suffix annual">/ Year</span>
+                                        <span class="price-suffix lifetime">/ Billed once</span>
                                         <span class="support-text">Advanced Features with More Flexibility</span>
                                     </div>
                                 </div>
@@ -292,8 +292,8 @@ $elite_features = [
                                         <span class="price-value lifetime five">199.99</span>
                                         <span class="price-value lifetime hundred">239.99</span>
 
-                                        <span class="price-suffix annual">/ Billed once</span>
-                                        <span class="price-suffix lifetime ">/ Year</span>
+                                        <span class="price-suffix annual">/ Year</span>
+                                        <span class="price-suffix lifetime ">/ Billed once</span>
                                         <span class="support-text">Most Comprehensive Set of Features</span>
                                     </div>
 
