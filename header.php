0864a4929527a2f4d58ae0b75786bb0d384f562f
Prince Ahmed <hitprince99@gmail.com>
2023-06-21 12:14:44 +0600

    Add conditional mobile rendering

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6DRHX4YG7"></script>
     <script>
@@ -14,92 +14,107 @@
         function gtag() {
             dataLayer.push(arguments);
         }
+
         gtag('js', new Date());
 
         gtag('config', 'G-C6DRHX4YG7');
     </script>
 
 
-
-
 </head>
 
 <?php
-$navbar_scheme   = get_theme_mod('navbar_scheme', 'navbar-light'); // Get custom meta-value.
-$navbar_position = get_theme_mod('navbar_position', 'static'); // Get custom meta-value.
+$navbar_scheme   = get_theme_mod( 'navbar_scheme', 'navbar-light' ); // Get custom meta-value.
+$navbar_position = get_theme_mod( 'navbar_position', 'static' ); // Get custom meta-value.
 
-$search_enabled = get_theme_mod('search_enabled', '1'); // Get custom meta-value.
+$search_enabled = get_theme_mod( 'search_enabled', '1' ); // Get custom meta-value.
 ?>
 
 <body <?php body_class(); ?>>
 
 
-    <?php wp_body_open(); ?>
+<?php wp_body_open(); ?>
 
-    <a href="#main" class="visually-hidden-focusable"><?php esc_html_e('Skip to main content', 'softlab'); ?></a>
+<a href="#main" class="visually-hidden-focusable"><?php esc_html_e( 'Skip to main content', 'softlab' ); ?></a>
 
-    <!-- Sticky Notification -->
-
-
-    <?php
-    $page_template = get_page_template_slug();
-
-    if ($page_template) {
-        get_template_part('template-parts/page', $page_template);
-    }
-
-    ?>
-
-    <div id="wrapper">
+<!-- Sticky Notification -->
 
 
-        <header class="site-header">
-            <div class="container">
-                <div class="row">
-                    <nav class="navbar navbar-expand-md primary-nav <?php echo esc_attr($navbar_scheme);
-                                                                    if (isset($navbar_position) && 'fixed_top' === $navbar_position) : echo ' fixed-top';
+<?php
+$page_template = get_page_template_slug();
 
-                                                                    elseif (isset($navbar_position) && 'fixed_bottom' === $navbar_position) : echo ' fixed-bottom';
-                                                                    endif;
-                                                                    if (is_home() || is_front_page()) : echo ' home';
-                                                                    endif; ?>">
+if ( $page_template ) {
+	get_template_part( 'template-parts/page', $page_template );
+}
 
-                        <a class="navbar-brand brand-website" href="<?php echo esc_url(home_url()); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
+?>
 
-                            <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/softlab-logo.png" alt="site-logo">
-                        </a>
+<div id="wrapper">
 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle navigation', 'softlab'); ?>">
-                            <span class="navbar-toggler-icon"></span>
 
-                        </button>
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+                <nav class="navbar navbar-expand-md primary-nav <?php echo esc_attr( $navbar_scheme );
+				if ( isset( $navbar_position ) && 'fixed_top' === $navbar_position ) : echo ' fixed-top';
 
-                        <div id="navbar" class=" mobile-menu collapse navbar-collapse">
+                elseif ( isset( $navbar_position ) && 'fixed_bottom' === $navbar_position ) : echo ' fixed-bottom';
+				endif;
+				if ( is_home() || is_front_page() ) : echo ' home';
+				endif; ?>">
+
+                    <a class="navbar-brand brand-website" href="<?php echo esc_url( home_url() ); ?>"
+                       title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+
+                        <img class="img-fluid"
+                             src="<?php echo get_template_directory_uri(); ?>/assets/images/softlab-logo.png"
+                             alt="site-logo">
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+                            aria-controls="navbar" aria-expanded="false"
+                            aria-label="<?php esc_attr_e( 'Toggle navigation', 'softlab' ); ?>">
+                        <span class="navbar-toggler-icon"></span>
+
+                    </button>
+
+                    <div id="navbar" class=" mobile-menu collapse navbar-collapse">
+
+						<?php if ( wp_is_mobile() ) { ?>
                             <span class="mobile-close-btn d-flex justify-content-between align-items-center">
-                                <a class="navbar-brand brand-website logo-btn" href="<?php echo esc_url(home_url()); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home">
+                                <a class="navbar-brand brand-website logo-btn"
+                                   href="<?php echo esc_url( home_url() ); ?>"
+                                   title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 
-                                    <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/assets/images/softlab-logo.png" alt="site-logo">
+                                    <img class="img-fluid"
+                                         src="<?php echo get_template_directory_uri(); ?>/assets/images/softlab-logo.png"
+                                         alt="site-logo">
                                 </a>
-                                <button id="close_btn" data-bs-toggle="collapse" data-bs-target="#navbar" class="close-btn"><i class="fa-solid fa-xmark"></i></button>
+                                <button id="close_btn" data-bs-toggle="collapse" data-bs-target="#navbar"
+                                        class="close-btn"><i class="fa-solid fa-xmark"></i></button>
                             </span>
-                            <?php
-                            wp_nav_menu(
-                                array(
-                                    'theme_location' => 'main-menu',
-                                    'container'      => '',
-                                    'menu_class'     => 'navbar-nav ms-auto',
-                                    'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
-                                    'walker'         => new WP_Bootstrap_Navwalker(),
-                                )
-                            );
-                            ?>
+						<?php } ?>
 
-                            <a class="nav-link btn text-white view-products-btn" href="/account/"><i class="fa-sharp fa-solid fa-user"></i> My account</a>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'main-menu',
+								'container'      => '',
+								'menu_class'     => 'navbar-nav ms-auto',
+								'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
+								'walker'         => new WP_Bootstrap_Navwalker(),
+							)
+						);
+						?>
 
-                        </div><!-- /.navbar-collapse -->
-                    </nav><!-- /#header -->
-                </div>
+                        <a class="nav-link btn text-white view-products-btn" href="/account/"><i
+                                    class="fa-sharp fa-solid fa-user"></i> My account</a>
+
+                    </div><!-- /.navbar-collapse -->
+
+                </nav><!-- /#header -->
             </div>
-        </header>
+        </div>
+    </header>
 
-        <main id="main" class="site-main">
+    <main id="main" class="site-main">
--- template-parts/home/hero.php
+++ template-parts/home/hero.php
@@ -5,11 +5,17 @@
             <div class="col-lg-6 col-md-6">
                 <div class="hero-content">
                     <h1>We Build Awesome Solutions for WordPress</h1>
-                    <div class="hero-image-mobile text-center mb-5">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/hero-image1.png" alt="" class="img-fluid w-75 w-md-100 ">
-                    </div>
+
+					<?php if ( wp_is_mobile() ) { ?>
+                        <div class="hero-image-mobile text-center mb-5">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/hero-image1.png"
+                                 alt="" class="img-fluid w-75 w-md-100 ">
+                        </div>
+					<?php } ?>
+
                     <p>
-                        We Develop Identical & Creative Products to Make WordPress Better, Faster, and Easier. Over 10K+ Happy Customers Who Exhibited Business to Next-Level from Our Products.
+                        We Develop Identical & Creative Products to Make WordPress Better, Faster, and Easier. Over 10K+
+                        Happy Customers Who Exhibited Business to Next-Level from Our Products.
                     </p>
                     <a href="/products" class="btn btn-primary view-products-btn"> <span></span> View Products</a>
 
@@ -18,7 +24,8 @@
 
             <div class="col-lg-6 col-md-6">
                 <div class="hero-image text-center text-md-end mt-5 mt-md-0">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/hero-image1.png" alt="" class="img-fluid w-75 w-md-100 ">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home/hero-image1.png" alt=""
+                         class="img-fluid w-75 w-md-100 ">
                 </div>
 
             </div>
