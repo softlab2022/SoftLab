1c6e6cf175583c549ee75f65ed4d65743d645318
akash29 <90980196+akashahmed29@users.noreply.github.com>
2023-09-10 12:25:41 +0600

    update dracula hero section of mobile devise

--- template-parts/dracula/home/hero.php
+++ template-parts/dracula/home/hero.php
@@ -3,7 +3,47 @@
         <div class="row align-items-center">
             <div class="col-lg-6 col-md-6">
                 <div class="section-head">
+
                     <h1>WordPress <span>Dark Mode Plugin</span> to Ready your Site for the Night</h1>
+
+                    <?php if (wp_is_mobile()) { ?>
+                        <div class="hero-image text-center">
+                            <div class="header-img text-center">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dracula/home/hero-dark.png" class="dracula-hero-image img-fluid w-75 w-md-100 ">
+                                <div class="hero-how-butt ripple">
+
+                                    <a class="my-btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                                        <i class="fa fa-play"></i>
+                                    </a>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <iframe width="900" height="500" src="https://www.youtube.com/embed/OHpY6X1Ha9g?si=nn0wvtK4188yPYjp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="demo-control text-center">
+                                <button class="mode-btn mode-light">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dracula/home/light-mode.png" class="img-fluid">
+                                    <h6 class="btn-content">Light</h6>
+                                </button>
+                                <button class="mode-btn mode-dark active">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dracula/home/dark-mode.png" class="img-fluid">
+                                    <h6 class="btn-content">Dark</h6>
+                                </button>
+                            </div>
+                        </div>
+                    <?php } ?>
 
                     <p>Dark Mode helps to create a stunning eye-pleasing dark version for your WordPress website easily
                         and effectively</p>
@@ -13,7 +53,7 @@
             </div>
 
             <div class="col-lg-6 col-md-6 ">
-              
+                <div class="hero-imge-desktop">
                     <div class="header-img text-center">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/dracula/home/hero-dark.png" class="dracula-hero-image img-fluid w-75 w-md-100 ">
                         <div class="hero-how-butt ripple">
@@ -48,7 +88,8 @@
                             <h6 class="btn-content">Dark</h6>
                         </button>
                     </div>
-               
+                </div>
+
             </div>
 
         </div>
