fc15c6ff802d7e2f831e0e9753b5439aae34b3a3
akash29 <akashahmed6579@gmail.com>
2024-10-06 14:54:47 +0600

    update integrate google drive pricing page feature list section

--- template-parts/integrate-google-drive/pricing/feature-list.php
+++ template-parts/integrate-google-drive/pricing/feature-list.php
@@ -6,12 +6,7 @@ $features = [
 		'video'		  => '',
 		'pro'         => true,
 	],
-	[
-		'title'       => 'Allow Specific Folders',
-		'description' => 'Integrate Google Drive plugin offers a functionality to allow specific folders accessibility in the plugin. This functionality restricts unwanted access to the folders you don\'t want to share with everyone. Only the selected folders will be accessible through the plugin.',
-		'video'		  => '',
-		'pro'         => false,
-	],
+	
 	[
 		'title'       => 'Admin File Browser',
 		'description' => 'Display & manage your cloud files via a File Browser. The file browser can have a grid thumbnail and list view and can have a file uploader.',
@@ -50,7 +45,12 @@ $features = [
 		'pro'         => true,
 
 	],
-
+	[
+		'title'       => 'Allow Specific Folders',
+		'description' => 'Integrate Google Drive plugin offers a functionality to allow specific folders accessibility in the plugin. This functionality restricts unwanted access to the folders you don\'t want to share with everyone. Only the selected folders will be accessible through the plugin.',
+		'video'		  => '',
+		'pro'         => false,
+	],
 	[
 		'title'       => 'File Uploader - Module',
 		'description' => 'A standalone uploader box which allows You and also your users can upload files directly to your Google Drive account from your site. You can upload unlimited size of files.',
