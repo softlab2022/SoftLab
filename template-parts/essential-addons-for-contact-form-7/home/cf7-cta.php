111b47d2a228feef8303c2ca9ab7908bb51b5cdf
akash29 <akashahmed6579@gmail.com>
2024-10-27 18:12:20 +0600

    update cta title & description

--- template-parts/essential-addons-for-contact-form-7/home/cf7-cta.php
+++ template-parts/essential-addons-for-contact-form-7/home/cf7-cta.php
@@ -3,14 +3,14 @@
         <div class="row align-items-center">
             <div class="col-lg-7">
                 <div class="cta-content text-center text-lg-start">
-                    <h3>Ready to Contact Form 7 Extended in Your website?</h3>
-                    <p>Best Contact Form 7 Extended plugin for WordPress to integrate, embed, download, browse, search, share your Google Drive </p>
+                    <h3>Ready to Enhance Your Contact Form 7 Functionality?</h3>
+                    <p>Essential Addons for Contact Form 7, adds powerful fields, features and customization options to make your form more creative</p>
                 </div>
             </div>
 
             <div class="col-lg-5 text-center text-lg-end">
                 <div class="cta-actions mt-5 mt-lg-0">
-                    <a href="" class="bt1">Try Live Demo</a>
+                    <a href="https://wordpress.org/plugins/essential-addons-for-contact-form-7/" class="bt1">Try Live Demo</a>
                     <a href="/essential-addons-for-contact-form-7-pricing/" class="bt">Buy Now</a>
                 </div>
             </div>
